0aa3dec9a0b279e5766ca9be7a56bb0623f15f6f
Ismail45364 <xxfaze.fyxx@gmail.com>
2022-11-14 11:35:30 +0700

    Commit

--- application/views/data_kelas.php
+++ application/views/data_kelas.php
@@ -26,7 +26,7 @@
                             </div>
                         </div>
                         <div class="card-body">
-                            <table class="table table-striped">
+                            <table id="data_kelas" class="table table-striped">
                                 <thead>
                                     <tr>
                                         <th>No</th>
