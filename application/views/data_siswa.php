a3b12929b1f1799c97b109b7f26c2431e827f71e
Ismail45364 <xxfaze.fyxx@gmail.com>
2022-11-28 11:20:31 +0700

    Upload

--- application/views/data_siswa.php
+++ application/views/data_siswa.php
@@ -56,3 +56,40 @@
             </div>
         </div>
     </div>
+    <!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document">
+    <div class="modal-content">
+      <div class="modal-header bg-primary">
+        <h5 class="modal-title" id="exampleModalLabel"><i class="fas fa-plus-circle"></i> Tambah Data Kelas</h5>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <div class="mb-3 row">
+          <label for="nama_kelas" class="col-sm-4 col-form-label">Kelas</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" id="kelas">
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label for="nama_kelas" class="col-sm-4 col-form-label">Jurusan</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" id="jurusan">
+          </div>
+        </div>
+        <div class="mb-3 row">
+          <label for="nama_kelas" class="col-sm-4 col-form-label">Jumlah Siswa</label>
+          <div class="col-sm-8">
+            <input type="text" class="form-control" id="jSiswa">
+          </div>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+        <button type="button" class="btn btn-primary">Save changes</button>
+      </div>
+    </div>
+  </div>
+</div>
